dd2172564a42a2d54deeb34a1ced13b038b1093c
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-03-17 21:20:58 +0100

    first commit

---
+++ mod.test.ts
@@ -0,0 +1,74 @@
+import { assertEquals } from 'https://deno.land/std@v0.180.0/testing/asserts.ts'
+import fetch from './mod.ts'
+
+Deno.test('client', async () => {
+  const client = new fetch()
+
+  const customClient = new fetch({
+    base: 'https://gist.githubusercontent.com/unvented',
+  })
+
+  // invalid
+  const invalid = await client.get('http/invalid', { type: 'text' })
+
+  assertEquals(invalid.data, undefined)
+  assertEquals(invalid.code, 0)
+  assertEquals(invalid.ok, false)
+  assertEquals(invalid.error, { code: 0, message: 'Network Error' })
+
+  // json
+  const json = await customClient.get(
+    '/6c83bf66f9210a5be30cf200bb58cb72/raw/767503c2ef8dba39b1f4d2be186a9f1fee005ac7/hello_world.json',
+    { type: 'json' },
+  )
+
+  assertEquals(json.data, { hello: 'world' })
+  assertEquals(json.code, 200)
+  assertEquals(json.ok, true)
+  assertEquals(json.error, undefined)
+
+  // text
+  const text = await customClient.get(
+    '/860490a5fc765eb737024a054c5436f1/raw/47d40dfcc39217ff5ce6bbce352a078dbc37d9aa/hello_world.txt',
+    { type: 'text' },
+  )
+
+  assertEquals(text.data, 'hello world')
+  assertEquals(text.code, 200)
+  assertEquals(text.ok, true)
+  assertEquals(text.error, undefined)
+
+  // array buffer
+  const arrayBuffer = await customClient.get(
+    '/860490a5fc765eb737024a054c5436f1/raw/47d40dfcc39217ff5ce6bbce352a078dbc37d9aa/hello_world.txt',
+    { type: 'buffer' },
+  )
+
+  assertEquals(arrayBuffer.data instanceof ArrayBuffer, true)
+  assertEquals(arrayBuffer.code, 200)
+  assertEquals(arrayBuffer.ok, true)
+  assertEquals(arrayBuffer.error, undefined)
+
+  // stream
+  const stream = await customClient.get(
+    '/860490a5fc765eb737024a054c5436f1/raw/47d40dfcc39217ff5ce6bbce352a078dbc37d9aa/hello_world.txt',
+    { type: 'stream' },
+  )
+
+  assertEquals(stream.data instanceof ReadableStream, true)
+  assertEquals(stream.code, 200)
+  assertEquals(stream.ok, true)
+  assertEquals(stream.error, undefined)
+
+  await stream.data?.cancel() // fails without that line
+
+  // does not exist
+  const doesNotExist = await customClient.get('/does-not-exist.txt', {
+    type: 'text',
+  })
+
+  assertEquals(doesNotExist.data, undefined)
+  assertEquals(doesNotExist.code, 404)
+  assertEquals(doesNotExist.ok, false)
+  assertEquals(doesNotExist.error, { code: 404, message: 'Not Found' })
+})
---
+++ mod.ts
@@ -0,0 +1,332 @@
+type Type =
+  | 'text'
+  | 'json'
+  | 'buffer'
+  | 'stream'
+
+type Data =
+  | string
+  | Record<string, unknown>
+  | ArrayBuffer
+  | ReadableStream<Uint8Array>
+
+class Fetcher {
+  private base: string | undefined
+  private headers: Record<string, string> | undefined
+
+  constructor(options?: {
+    base: string
+    headers?: Record<string, string>
+  }) {
+    this.base = options?.base
+    this.headers = options?.headers
+  }
+
+  async fetch<T>(
+    method: string,
+    url: string,
+    options: {
+      onSuccess?: (
+        payload: {
+          data: T extends 'text' ? string
+            : T extends 'json'
+            // deno-lint-ignore no-explicit-any
+              ? Record<string, any>
+            : T extends 'buffer' ? ArrayBuffer
+            : ReadableStream<Uint8Array>
+          code: number
+          headers: Record<string, string>
+        },
+      ) => unknown
+      onError?: (
+        payload: {
+          error: { code: number; message: string }
+          code: number
+          headers: Record<string, string>
+        },
+      ) => unknown
+      headers?: Record<string, string>
+      data?:
+        | string
+        | Record<string, unknown>
+        | ArrayBuffer
+        | ReadableStream<Uint8Array>
+      type:
+        | 'text'
+        | 'json'
+        | 'buffer'
+        | 'stream'
+    },
+  ): Promise<
+    {
+      data: T extends 'text' ? string
+        : T extends 'json'
+        // deno-lint-ignore no-explicit-any
+          ? Record<string, any>
+        : T extends 'buffer' ? ArrayBuffer
+        : ReadableStream<Uint8Array>
+      error: undefined
+      code: number
+      ok: true
+      headers: Record<string, string>
+    } | {
+      data: undefined
+      error: { code: number; message: string }
+      code: number
+      ok: false
+      headers: Record<string, string>
+    }
+  > {
+    try {
+      let body: BodyInit | undefined
+      let type: string | undefined
+
+      if (options.data) {
+        if (typeof options.data === 'string') {
+          body = options.data
+          type = 'text/plain; charset=utf-8;'
+        } else if (options.data instanceof ArrayBuffer) {
+          body = options.data
+        } else if (options.data instanceof ReadableStream) {
+          body = options.data
+        } else {
+          body = JSON.stringify(options.data)
+          type = 'application/json; charset=utf-8;'
+        }
+      }
+
+      const res = await fetch(this.base ? `${this.base}${url}` : url, {
+        ...(body && { body }),
+        method,
+        headers: {
+          ...(type && { 'content-type': type }),
+          ...options.headers,
+          ...this.headers,
+        },
+      })
+
+      const headers: Record<string, string> = {}
+
+      for (const [key, value] of res.headers) {
+        headers[key.toLowerCase()] = value
+      }
+
+      const def = { code: res.status, ok: res.ok, headers }
+
+      if (res.ok) {
+        const data = options.type === 'buffer'
+          ? await res.arrayBuffer()
+          : options.type === 'json'
+          ? await res.json()
+          : options.type === 'stream'
+          ? res.body
+          : options.type === 'text'
+          ? await res.text()
+          : null
+
+        if (!options.onSuccess) {
+          return {
+            data,
+            error: undefined,
+            code: res.status,
+            ok: res.ok,
+            headers,
+          }
+        }
+
+        options.onSuccess({ data, code: res.status, headers })
+      }
+
+      const error = res.headers.get('content-type')?.includes('json')
+        ? await res.json()
+        : { code: res.status, message: await res.text() }
+
+      if (!options.onError) {
+        return { data: undefined, error, code: res.status, ok: false, headers }
+      }
+
+      options.onError({ error, ...def })
+
+      // deno-lint-ignore no-explicit-any
+      return undefined as any
+    } catch (_err) {
+      return {
+        data: undefined,
+        code: 0,
+        error: {
+          code: 0,
+          message: 'Network Error',
+        },
+        ok: false,
+        headers: {},
+      }
+    }
+  }
+
+  async get<T extends Type>(url: string, options: {
+    headers?: Record<string, string>
+    type: T
+    onSuccess?: (
+      payload: {
+        data:
+          | (T extends 'text' ? string
+            : T extends 'json'
+            // deno-lint-ignore no-explicit-any
+              ? Record<string, any>
+            : T extends 'buffer' ? ArrayBuffer
+            : ReadableStream<Uint8Array>)
+          | undefined
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+    onError?: (
+      payload: {
+        error: { code: number; message: string }
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+  }) {
+    return await this.fetch<T>('GET', url, options)
+  }
+
+  async post<T extends Type>(url: string, options: {
+    data?: Data
+    headers?: Record<string, string>
+    type: T
+    onSuccess?: (
+      payload: {
+        data:
+          | (T extends 'text' ? string
+            : T extends 'json'
+            // deno-lint-ignore no-explicit-any
+              ? Record<string, any>
+            : T extends 'buffer' ? ArrayBuffer
+            : ReadableStream<Uint8Array>)
+          | undefined
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+    onError?: (
+      payload: {
+        error: { code: number; message: string }
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+  }) {
+    return await this.fetch<T>('POST', url, options)
+  }
+
+  async put<T extends Type>(url: string, options: {
+    data?: Data
+    headers?: Record<string, string>
+    type: T
+    onSuccess?: (
+      payload: {
+        data:
+          | (T extends 'text' ? string
+            : T extends 'json'
+            // deno-lint-ignore no-explicit-any
+              ? Record<string, any>
+            : T extends 'buffer' ? ArrayBuffer
+            : ReadableStream<Uint8Array>)
+          | undefined
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+    onError?: (
+      payload: {
+        error: { code: number; message: string }
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+  }) {
+    return await this.fetch<T>('PUT', url, options)
+  }
+
+  async patch<T extends Type>(url: string, options: {
+    data?: Data
+    headers?: Record<string, string>
+    type: T
+    onSuccess?: (
+      payload: {
+        data:
+          | (T extends 'text' ? string
+            : T extends 'json'
+            // deno-lint-ignore no-explicit-any
+              ? Record<string, any>
+            : T extends 'buffer' ? ArrayBuffer
+            : ReadableStream<Uint8Array>)
+          | undefined
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+    onError?: (
+      payload: {
+        error: { code: number; message: string }
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+  }) {
+    return await this.fetch<T>('PATCH', url, options)
+  }
+
+  async delete<T extends Type>(url: string, options: {
+    data?: Data
+    headers?: Record<string, string>
+    type: T
+    onSuccess?: (
+      payload: {
+        data:
+          | (T extends 'text' ? string
+            : T extends 'json'
+            // deno-lint-ignore no-explicit-any
+              ? Record<string, any>
+            : T extends 'buffer' ? ArrayBuffer
+            : ReadableStream<Uint8Array>)
+          | undefined
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+    onError?: (
+      payload: {
+        error: { code: number; message: string }
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+  }) {
+    return await this.fetch<T>('DELETE', url, options)
+  }
+
+  async head(url: string, options: {
+    headers?: Record<string, string>
+    onSuccess?: (
+      payload: {
+        data: string
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+    onError?: (
+      payload: {
+        error: { code: number; message: string }
+        code: number
+        headers: Record<string, string>
+      },
+    ) => unknown
+  }) {
+    // @ts-ignore: ok
+    return await this.fetch('HEAD', url, { ...options, type: 'text' })
+  }
+}
+
+export default Fetcher
